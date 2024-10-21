c692bd0c70024875d236ba0a90aae32e176bd004
Abdul-Jobber-2407 <arjobbar2019@gmail.com>
2024-10-21 15:49:24 +0600

    first commit

---
+++ test.js
@@ -0,0 +1,25 @@
+const  price = 45;
+
+// function declare
+function fanoffkor(){
+    console.log('bosa theke uthe dara');
+    console.log('walk towards the swich');
+    console.log('click the switch to off of the fan');
+}
+
+// call the function
+fanoffkor();
+// function name L(){
+
+// }
+
+// call the function
+
+
+function brushTeeth(){
+    console.log('pick up the brush');
+    console.log('add paste');
+    console.log('2 min goso and goso fena felo');
+
+}
+brushTeeth();
