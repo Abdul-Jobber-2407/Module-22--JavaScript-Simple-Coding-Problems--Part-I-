f76fcbf8ab1adc074fb780fcaff52028f0a0b87d
Abdul-Jobber-2407 <arjobbar2019@gmail.com>
2024-10-23 10:21:09 +0600

    added to a final push

--- test.js
+++ test.js
@@ -1,25 +1,6 @@
-const  price = 45;
-
-// function declare
-function fanoffkor(){
-    console.log('bosa theke uthe dara');
-    console.log('walk towards the swich');
-    console.log('click the switch to off of the fan');
+function kiloMeterToMiles (kilo){
+    const mile = kilo * 0.621371;
+    return mile;
 }
-
-// call the function
-fanoffkor();
-// function name L(){
-
-// }
-
-// call the function
-
-
-function brushTeeth(){
-    console.log('pick up the brush');
-    console.log('add paste');
-    console.log('2 min goso and goso fena felo');
-
-}
-brushTeeth();
+const convert = kiloMeterToMiles (4);
+console.log(convert);
